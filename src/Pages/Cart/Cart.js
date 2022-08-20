e4371615f67cb0632ff512f687b1c6c4a6c1f5d8
Anirbanjyoti <anirbanjyoti708@gmail.com>
2022-08-20 14:04:43 +0530

    total price

--- src/Pages/Cart/Cart.js
+++ src/Pages/Cart/Cart.js
@@ -1,10 +1,18 @@
 import React from 'react';
 
 const Cart = ({cart}) => {
+    let total = 0;
+    for(const product of cart){
+        total = total + product.price;
+    } 
+    
     return (
         <div>
               <h1 className="text-3xl ">Order Summery</h1>
           <p>Selected Item: {cart.length}</p>
+          <p>Total Price: {total}</p>
+          <p>Total Shipping: </p>
+          <p>Selected Item: </p>
         </div>
     );
 };
