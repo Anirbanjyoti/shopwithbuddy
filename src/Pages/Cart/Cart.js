4544bf44b1a5ab31100a8cce14a8b25ab9eda3d8
Anirbanjyoti <anirbanjyoti708@gmail.com>
2022-08-20 22:23:43 +0530

    cart system done

--- src/Pages/Cart/Cart.js
+++ src/Pages/Cart/Cart.js
@@ -3,9 +3,10 @@ import React from "react";
 const Cart = ({ cart }) => {
   let total = 0;
   let shipping = 0;
-
+  let quantity = 0;
   for (const product of cart) {
-    total = total + product.price;
+    quantity = quantity + product.quantity;
+    total = total + product.price * product.quantity;
     shipping = shipping + product.shipping;
   }
   let tax = parseFloat((total * 0.1).toFixed(2));
@@ -13,7 +14,7 @@ const Cart = ({ cart }) => {
   return (
     <div>
       <h1 className="text-3xl ">Order Summery</h1>
-      <p>Selected Item: {cart.length}</p>
+      <p>Selected Item: {quantity}</p>
       <p>Total Price: ${total}</p>
       <p>Total Shipping:{shipping} </p>
       <p>Tax: {tax}</p>
--- src/Pages/Shop.js
+++ src/Pages/Shop.js
@@ -12,39 +12,55 @@ const Shop = () => {
       .then((data) => setProducts(data));
   }, []);
   // pick data from localStorage
-  useEffect(()=>{
+  useEffect(() => {
     const storedCart = getStoredCart();
     const savedCart = [];
-    for(const _id in storedCart){
-      const addedProduct = products.find(p=>p._id===_id)
+    for (const _id in storedCart) {
+      const addedProduct = products.find((p) => p._id === _id);
       console.log(addedProduct);
-      if(addedProduct){
+      if (addedProduct) {
         const quantity = storedCart[_id];
         addedProduct.quantity = quantity;
         savedCart.push(addedProduct);
       }
-      
     }
     setCart(savedCart);
-  }, [products])
-  const handleAddToCart=(p)=>{
-    const newCart = [...cart, p]
-    setCart(newCart)
-    addToDb(p._id);
-  }
+  }, [products]);
+
+  const handleAddToCart = (selectedProduct) => {
+    console.log(selectedProduct);
+    let newCart = [];
+    const exists = cart.find((p) => p._id === selectedProduct._id);
+    if (!exists) {
+      selectedProduct.quantity = 1;
+      newCart = [...cart, selectedProduct];
+    } else {
+      const rest = cart.filter(
+        (p) => p._id !== selectedProduct._id
+      );
+      exists.quantity = exists.quantity + 1;
+      newCart = [...rest, exists];
+    }
+    setCart(newCart);
+    addToDb(selectedProduct._id);
+  };
   return (
     <div>
       <div class="flex flex row justify-center p-20 pr-0">
         <div className="lg:basis-3/4 md:basis-3/4 sm:basis-full">
           <div className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-10">
             {products.map((p) => (
-              <Product key={p._id} p={p} handleAddToCart={handleAddToCart}></Product>
+              <Product
+                key={p._id}
+                p={p}
+                handleAddToCart={handleAddToCart}
+              ></Product>
             ))}
           </div>
         </div>
 
         <div className="lg:basis-1/4 md:basis-1/4 sm:basis-full xs:basis-full border-2 mx-10 rounded-lg text-center pt-10">
-        <Cart cart={cart}></Cart>
+          <Cart cart={cart}></Cart>
         </div>
       </div>
     </div>
