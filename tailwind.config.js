2fe3f4488c5565a085dc77b65c9510a02e7b5e03
Anirbanjyoti <anirbanjyoti708@gmail.com>
2022-08-20 09:01:00 +0530

    shopwithbuddy create app and navbar

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  daisyui: {
+    themes: [
+      {
+        partstheme: {
+          primary: "#EAEAEA",
+          secondary: "#C7B340",
+          accent: "#3A4256",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
+  plugins: [require("daisyui")],
+}
