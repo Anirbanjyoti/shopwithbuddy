07baf89d451c10312f08cdfd1d5bb09addcf0627
Anirbanjyoti <anirbanjyoti708@gmail.com>
2022-08-20 14:38:53 +0530

    install jwt

--- src/Pages/Cart/Cart.js
+++ src/Pages/Cart/Cart.js
@@ -1,22 +1,25 @@
-import React from 'react';
+import React from "react";
 
-const Cart = ({cart}) => {
-    let total = 0;
-    let shipping = 0;
-    for(const product of cart){
-        total = total + product.price;
-        shipping = shipping + product.shipping;
-    } 
-    
-    return (
-        <div>
-              <h1 className="text-3xl ">Order Summery</h1>
-          <p>Selected Item: {cart.length}</p>
-          <p>Total Price: {total}</p>
-          <p>Total Shipping:{shipping} </p>
-          <p>Selected Item: </p>
-        </div>
-    );
+const Cart = ({ cart }) => {
+  let total = 0;
+  let shipping = 0;
+
+  for (const product of cart) {
+    total = total + product.price;
+    shipping = shipping + product.shipping;
+  }
+  let tax = parseFloat((total * 0.1).toFixed(2));
+  const gradTotal = total + shipping + tax;
+  return (
+    <div>
+      <h1 className="text-3xl ">Order Summery</h1>
+      <p>Selected Item: {cart.length}</p>
+      <p>Total Price: ${total}</p>
+      <p>Total Shipping:{shipping} </p>
+      <p>Tax: {tax}</p>
+      <p>Gran Total: {gradTotal.toFixed(2)}</p>
+    </div>
+  );
 };
 
 export default Cart;
