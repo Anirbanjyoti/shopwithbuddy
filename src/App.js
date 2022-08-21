074dbc4e28a439e032e68d771a8d1591fbd61589
Anirbanjyoti <anirbanjyoti708@gmail.com>
2022-08-21 12:00:08 +0530

    create inventory private route

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 // import logo from './logo.svg';
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
-import About from './Pages/About';
-import Inventory from './Pages/Inventory';
-import Login from './Pages/Login/Login';
-import SignUp from './Pages/Login/SignUp';
-import Orders from './Pages/Orders';
-import Navbar from './Pages/Sheared/Navbar';
-import Shop from './Pages/Shop';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import About from "./Pages/About";
+import Inventory from "./Pages/Inventory";
+import Login from "./Pages/Login/Login";
+import RequireAuth from "./Pages/Login/RequireAuth";
+import SignUp from "./Pages/Login/SignUp";
+import Orders from "./Pages/Orders";
+import Navbar from "./Pages/Sheared/Navbar";
+import Shop from "./Pages/Shop";
 
 function App() {
   return (
     <div>
-    <Navbar></Navbar>
+      <Navbar></Navbar>
       <Routes>
-      <Route path="/" element={<Shop />} />
+        <Route path="/" element={<Shop />} />
         <Route path="/shop" element={<Shop />} />
         <Route path="/orders" element={<Orders />} />
         <Route path="/about" element={<About />} />
-        <Route path="/inventory" element={<Inventory />} />
+        <Route
+          path="/inventory"
+          element={
+            <RequireAuth>
+              <Inventory />
+            </RequireAuth>
+          }
+        />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
       </Routes>
